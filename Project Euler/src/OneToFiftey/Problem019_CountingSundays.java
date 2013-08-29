630ec37011f09ba17cc9003f7f43711de15c26ce
Nick Tollefson <zapper985@gmail.com>
2013-08-29 12:30:10 -0400

    GOGOGo

--- Project Euler/src/OneToFiftey/Problem019_CountingSundays.java
+++ Project Euler/src/OneToFiftey/Problem019_CountingSundays.java
@@ -2,22 +2,29 @@ class Problem019_CountingSundays{
 
     // 0 - M, 1 - T, 2 - W, 3- Th,4 - F, 5 - Su, 6 - S
     
-    int dayOfWeek,dayOfYear,year;
+    int dayOfWeek = 0, dayOfYear = 0, year = 0, numberOfSundays = 0;
     boolean 20thCentuary = true;
     
-    while (20thCentuary == true)
-    if (year == 100)
-        break;
-    if (dayOfWeek == 7)
-        dayOfWeek = 0;
-    if (dayOfYear => 365){
-        if ((year % 4)== 0)
-            break;
-        else {
-            dayOfYear = 1;
-            year++;
+    while (20thCentuary == true) {
+        dayOfWeek++;
+        dayOfYear++;
+        if (year == 100)
+            20thCentuary == false;
+        if (dayOfWeek == 7)
+            dayOfWeek = 0;
+        if (dayOfYear => 365){
+            if ((year % 4) == 0)
+                break;
+            else {
+                dayOfYear = 1;
+                year++;
+            }
         }
-    if
+        if (dayOfYear =< 31) {
+            if (dayOfWeek == 5) {
+                numberOfSundays ++;
+            }
+        System.out.println( numberOfSundays + ":" year )
+        }
     }
-    
 }
