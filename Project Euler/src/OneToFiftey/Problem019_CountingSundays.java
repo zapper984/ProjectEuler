cbc4225930720387711d61443a221f8fd8ffd586
Nick Tollefson <zapper985@gmail.com>
2013-08-29 12:21:56 -0400

    Its goin

--- Project Euler/src/OneToFiftey/Problem019_CountingSundays.java
+++ Project Euler/src/OneToFiftey/Problem019_CountingSundays.java
@@ -1,3 +1,23 @@
-class (Problem019_CountingSundays){
+class Problem019_CountingSundays{
 
+    // 0 - M, 1 - T, 2 - W, 3- Th,4 - F, 5 - Su, 6 - S
+    
+    int dayOfWeek,dayOfYear,year;
+    boolean 20thCentuary = true;
+    
+    while (20thCentuary == true)
+    if (year == 100)
+        break;
+    if (dayOfWeek == 7)
+        dayOfWeek = 0;
+    if (dayOfYear => 365){
+        if ((year % 4)== 0)
+            break;
+        else {
+            dayOfYear = 1;
+            year++;
+        }
+    if
+    }
+    
 }
